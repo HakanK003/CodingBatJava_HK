01e2898277056f82716d458f6896cfc841f58792
HakanK003 <hkaratashk3@gmail.com>
2022-03-19 21:50:26 -0400

    Old versions

---
+++ src/warmup1_Solutions/makes10.java
@@ -0,0 +1,15 @@
+package warmup1_Solutions;
+
+public class makes10 {
+//    public boolean makes10(int a, int b) {
+//
+//    }
+}
+
+/*
+Given 2 ints, a and b, return true if one if them is 10 or if their sum is 10.
+
+makes10(9, 10) → true
+makes10(9, 9) → false
+makes10(1, 9) → true
+ */
